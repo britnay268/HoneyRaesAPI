d2d6d5f38c17160f806c8a1d1b1feca3670d8025
Britnay Gore <britnaygore268@gmail.com>
2024-07-22 21:43:10 -0400

    Added properties to the data models

---
+++ HoneyRaesAPI/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HoneyRaesAPI.Models;
+
+public class Employee
+{
+	public int Id { get; set; }
+	public string? Name { get; set; }
+	public string? Speciality { get; set; }
+}
+
