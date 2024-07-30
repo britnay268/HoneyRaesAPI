9a4e7c7d9894753f4ddbf1a79c419bb84d653ba8
Britnay Gore <britnaygore268@gmail.com>
2024-07-29 20:07:09 -0400

    Added code to ignore cycling issues

--- HoneyRaesAPI/Program.cs
+++ HoneyRaesAPI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HoneyRaesAPI.Models;
 using System.Collections.Generic;
 using System;
+using System.Text.Json.Serialization;
 
 List<Customer> customers = new List<Customer>
 {
@@ -144,6 +145,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
